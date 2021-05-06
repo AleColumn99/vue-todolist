33614d9ba01e73c957edf869dc7044225e8ce970
AleColumn99 <alecolumn.99@gmail.com>
2021-05-06 16:22:19 +0200

    esercizio - no bonus

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const app = new Vue ({
+
+  el: '#app',
+  data: {
+
+    alertActive: false,
+    todos: [
+      {
+        todo: 'Ripassare HTML/CSS',
+        checked: false
+      },
+      {
+        todo: 'Studiare Vue',
+        checked: false
+      },
+      {
+        todo: 'Amare Vue',
+        checked: true
+      },
+      {
+        todo: 'Alzarsi presto la mattina per ripassare',
+        checked: false
+      },
+    ]
+
+  },
+  methods: {
+
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    addTodo() {
+      if (this.strTodo.length > 2) {
+        this.todos.push({
+          todo: this.strTodo,
+          checked: false
+        });
+        this.strTodo = '';
+      } else {
+        this.alertActive = true;
+        this.strTodo = '';
+        setTimeout(() => {
+          this.alertActive = false;
+        }, 2000);
+      }
+    }
+
+  }
+
+});
